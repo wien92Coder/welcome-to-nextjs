10c70052500c9b92f4f2d6c88379e008da6a1c22
wien92coder <winarno92@gmail.com>
2025-07-27 05:34:16 +0700

    Adding multiple fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-        <h1 className="text-3xl text-violet-700 font-black">Welcome to Next.js 👋</h1>
+        <h1 className="h1-bold">Welcome to Next.js 👋</h1>
     </div>
   );
 }
