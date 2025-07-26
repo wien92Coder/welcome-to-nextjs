1175c8be1ad9f1a94a86dbe049ceed2abe594d61
wien92coder <winarno92@gmail.com>
2025-07-26 14:57:04 +0700

    Implement ESLint & Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,65 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import { globalIgnores } from "eslint/config";
+import importPlugin from 'eslint-plugin-import';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+  plugins: {
+    import: importPlugin,
+  },
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  globalIgnores([
+    "components/ui/**",
+  ]),
+  {
+    rules: {
+      "no-undef": "off",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["sibling", "parent"],
+            "index",
+            "object",
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+    }
+  },
+  {
+    files: [
+      "*.ts",
+      "*.tsx",
+    ],
+    rules: {
+      "no-undef": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
