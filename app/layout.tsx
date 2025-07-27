31467a9e07ba1925960b05f16517b5cbd5644bf4
wien92coder <winarno92@gmail.com>
2025-07-28 06:26:35 +0700

    Shadcn, Light and Dark Theme, and Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import Navbar from "@/components/navigation/navbar";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -28,11 +30,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ components/navigation/navbar/index.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import Theme from './Theme'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-between background-light900_dark200 fixed z-50 w-full p-6 shadow-light-300 dark:shadow-none sm:px-12 gap-5'>
+      <Link href="/" className='flex items-center gap-1'>
+        <Image src="/images/site-logo.svg" alt="DevFlow Logo" width={23} height={23} />
+
+        <p className='h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden'>Dev<span className='text-primary-500'>Flow</span></p>
+      </Link>
+
+      <p>Global Search</p>
+
+      <div className='flex-between gap-5'><Theme /></div>
+    </nav>
+  )
+}
+
+export default Navbar
